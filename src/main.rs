70d3b517550001fc450f434e13673807c4a5efd3
kitagawa <kitadrum50@gmail.com>
2019-10-25 20:35:00 +0900

    3.2.7: add relu

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ pub fn step_function(x: Vec<f64>) -> Vec<f64> {
     x.into_iter().map(|el| if el > 0. { 1. } else { 0. }).collect()
 }
 
+pub fn relu(x: Vec<f64>) -> Vec<f64> {
+    x.into_iter().map(|el| if el > 0. { el } else { 0. }).collect()
+}
+
 fn main() {
     let x = (-500..500).map(|n| n as f64 / 100.).collect::<Vec<f64>>();
     let y = step_function(x.clone());
@@ -43,4 +47,20 @@ fn main() {
         .y_range(-0.5, 1.5);
 
     Page::single(&v).save("sigmoid.svg").unwrap();
+
+    let x = (-500..500).map(|n| n as f64 / 100.).collect::<Vec<f64>>();
+    let y = relu(x.clone());
+
+    let data = x.into_iter().enumerate().map(|(i, xd)| (xd, y[i])).collect::<Vec<(f64, f64)>>();
+
+    let l1 = Line::new(&data).style(
+        Style::new().colour("#DD3355"),
+    );
+
+    let v = ContinuousView::new()
+        .add(&l1)
+        .x_range(-5.5, 5.5)
+        .y_range(-0.5, 5.5);
+
+    Page::single(&v).save("relu.svg").unwrap();
 }
