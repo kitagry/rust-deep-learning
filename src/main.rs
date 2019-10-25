21edfd945c780eab514eeaae0ebac55912d5401d
kitagawa <kitadrum50@gmail.com>
2019-10-25 20:24:50 +0900

    3.2.4: sigmoid function

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use plotlib::line::{Line, Style};
 use plotlib::style::Line as LineStyle;
 use plotlib::view::ContinuousView;
 
-pub fn sigmoid(x: f64) -> f64 {
-    1. / (1. + x.exp())
+pub fn sigmoid(x: Vec<f64>) -> Vec<f64> {
+    x.into_iter().map(|el| 1. / (1. + (-el).exp())).collect()
 }
 
 pub fn step_function(x: Vec<f64>) -> Vec<f64> {
@@ -12,7 +12,7 @@ pub fn step_function(x: Vec<f64>) -> Vec<f64> {
 }
 
 fn main() {
-    let x = (-50..50).map(|n| n as f64 / 100.).collect::<Vec<f64>>();
+    let x = (-500..500).map(|n| n as f64 / 100.).collect::<Vec<f64>>();
     let y = step_function(x.clone());
 
     let data = x.into_iter().enumerate().map(|(i, xd)| (xd, y[i])).collect::<Vec<(f64, f64)>>();
@@ -23,8 +23,24 @@ fn main() {
 
     let v = ContinuousView::new()
         .add(&l1)
-        .x_range(-1., 1.)
+        .x_range(-5.5, 5.5)
         .y_range(-0.5, 1.5);
 
     Page::single(&v).save("step_function.svg").unwrap();
+
+    let x = (-500..500).map(|n| n as f64 / 100.).collect::<Vec<f64>>();
+    let y = sigmoid(x.clone());
+
+    let data = x.into_iter().enumerate().map(|(i, xd)| (xd, y[i])).collect::<Vec<(f64, f64)>>();
+
+    let l1 = Line::new(&data).style(
+        Style::new().colour("#DD3355"),
+    );
+
+    let v = ContinuousView::new()
+        .add(&l1)
+        .x_range(-5.5, 5.5)
+        .y_range(-0.5, 1.5);
+
+    Page::single(&v).save("sigmoid.svg").unwrap();
 }
