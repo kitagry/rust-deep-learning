bff76a991da9c5793d818ea0765fef5951a9beb3
kitagawa <kitadrum50@gmail.com>
2019-10-26 16:13:21 +0900

    Add mnist and save png

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate ndarray;
+extern crate mnist;
+extern crate image;
 
+use mnist::{MnistBuilder, Mnist};
 use ndarray::{array, Array1};
 
 pub fn sigmoid(x: Array1<f64>) -> Array1<f64> {
@@ -23,14 +26,35 @@ pub fn softmax(x: Array1<f64>) -> Array1<f64> {
 
 fn main() {
     // dim()で要素が分かる
-    let a = array![[1., 2., 3.], [4., 5., 6.]];
-    let b = array![[1., 2.], [3., 4.], [5., 6.]];
+    // let a = array![[1., 2., 3.], [4., 5., 6.]];
+    // let b = array![[1., 2.], [3., 4.], [5., 6.]];
 
-    println!("{:?}", a.dot(&b));
+    // println!("{:?}", a.dot(&b));
 
-    let a = array![1., 2., 3.];
-    println!("{:?}", sigmoid(a));
+    // let a = array![1., 2., 3.];
+    // println!("{:?}", sigmoid(a));
 
-    let a = array![1010., 1000., 990.];
-    println!("{:?}", softmax(a));
+    // let a = array![1010., 1000., 990.];
+    // println!("{:?}", softmax(a));
+
+    let (trn_size, rows, cols) = (50_000, 28, 28);
+
+    // Deconstruct the returned Mnist struct.
+    let Mnist { trn_img, trn_lbl, tst_img, tst_lbl, .. } = MnistBuilder::new()
+        .label_format_digit()
+        .training_set_length(trn_size)
+        .validation_set_length(10_000)
+        .test_set_length(10_000)
+        .finalize();
+
+    // Get the label of the first digit.
+    let first_label = trn_lbl[0];
+    println!("The first digit is a {}.", first_label);
+
+    println!("train image num: {}, test image num: {}", trn_img.len(), tst_img.len());
+    println!("test image num: {}", tst_img.len() / 28 / 28);
+
+    let mut buffer = &trn_img[0..28*28];
+
+    image::save_buffer("image.png", buffer, 28, 28, image::Gray(8)).unwrap();
 }
